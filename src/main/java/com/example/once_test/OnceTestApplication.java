ce3584a408e772f503ea15746203a9bcc52f5baf
YiLan21 <gsn4466@gmail.com>
2023-02-16 17:59:02 +0800

    firstt

---
+++ src/main/java/com/example/once_test/OnceTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.once_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnceTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/once_test/OnceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.once_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
